7767f86764f63315869ee30a29a8557fa8c492d1
luckytarun24 <luckyjaintj24@gmail.com>
2024-03-13 19:36:21 +0530

    update

--- src/webComponent.js
+++ src/webComponent.js
@@ -1,15 +1,25 @@
-class CustomButton extends HTMLElement {
+class WebComponent extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-    const btn = document.createElement('button');
-    btn.innerText = 'click me';
-    shadow.append(btn);
+    const btn = document.createElement("button");
+    btn.textContent = "Click me";
+    btn.addEventListener("click", () => {
+      console.log("I am custom web component")
+    })
+     
+    btn.style.border= "2px solid yellow";
+    btn.style.color="yellow";
+    btn.style.background="black";
+    btn.style.padding= "10px 20px";
+    
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.append(btn)
   }
 
-  // connectedCallback() {
-  //   this.shadowRoot.innerHTML = 'tarun';
-  // }
+  connectedCallback(){
+    // this.shadowRoot.append(this.btn) 
+  }
+
 }
 
-customElements.define('custom-btn', CustomButton);
+customElements.define("custom-btn", WebComponent)
